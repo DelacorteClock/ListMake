b363dc79ddb089358f84712623e1d80998ce0643
DelacorteClock <123141973+DelacorteClock@users.noreply.github.com>
2023-02-07 11:42:41 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+
+function newItem() {
+
+//javascript
+//1. Adding a new item to the list of items: 
+    var li = document.createElement('li');
+    var inputValue = document.getElementById('input').value;
+    var text = document.createTextNode(inputValue);
+    li.appendChild(text);
+
+    if (inputValue === '') {
+        alert('No Inputted Destination');
+    } else {
+        var list = document.querySelector('#list');
+        list.appendChild(li);
+    }
+
+    //2. Crossing out an item from the list of items:
+    function crossOut() {
+        li.classList.toggle('strike');
+    }
+
+    li.addEventListener('dblclick', crossOut);
+
+    //3(i). Adding the delete button 'X': 
+    var removeButton = document.createElement('removeButton');
+    removeButton.appendChild(document.createTextNode('X'));
+    li.appendChild(removeButton);
+    removeButton.addEventListener('click', deleteListItem);
+
+    //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+    function deleteListItem() {
+        li.classList.add('delete')
+    }
+    // 4. Reordering the items: 
+    $('#list').sortable();
+
+}
+
+function temp() {
+    var li = $('<li></li>');
+    var inputValue = $('#input').val();
+    li.append(inputValue);
+
+    if (inputValue === '') {
+        alert('You must write something!');
+    } else {
+        $('#list').append(li);
+    }
+
+}
